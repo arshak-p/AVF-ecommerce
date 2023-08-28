4dadae24646b26985d1ebd4fec7c0048c81c221c
arshak6410 <arshakyob@gmail.com>
2023-08-28 18:13:01 +0530

    final commit before updation

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,12 +1,12 @@
 from .models import CartItem,Cart
 
-def cart_item_count(request):
-    cart_item_count = 0
+# def cart_item_count(request):
+#     cart_item_count = 0
 
-    if request.user.is_authenticated:
-        cart_item_count = CartItem.objects.filter(user=request.user, is_active=True).count()
-    else:
-        cart = Cart.objects.get(session_id=_session_id(request))
-        cart_item_count = CartItem.objects.filter(cart=cart, is_active=True).count()
+#     if request.user.is_authenticated:
+#         cart_item_count = CartItem.objects.filter(user=request.user, is_active=True).count()
+#     else:
+#         # cart = Cart.objects.get(session_id=_session_id(request))
+#         # cart_item_count = CartItem.objects.filter(cart=cart, is_active=True).count()
 
-    return {'cart_item_count': cart_item_count}
+#     # return {'cart_item_count': cart_item_count}
